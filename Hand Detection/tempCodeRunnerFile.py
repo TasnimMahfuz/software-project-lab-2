21014fb3f9fa829dccce0f2379972e5a127e16b9
TasnimMahfuz <mahfuznafis1@gmail.com>
2024-04-25 16:55:14 +0600

    created bounding box

--- Hand Detection/tempCodeRunnerFile.py
+++ Hand Detection/tempCodeRunnerFile.py
@@ -1,14 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr 25 16:01:10 2024
-
-@author: nafis
-"""
-
 import cv2 
 import mediapipe as mp 
-from google.protobuf.json_format import MessageToDict
+  
+# Used to convert protobuf message to a dictionary. 
+from google.protobuf.json_format import MessageToDict 
   
 # Initializing the Model 
 mpHands = mp.solutions.hands 
@@ -36,48 +30,39 @@ while True:
     results = hands.process(imgRGB) 
   
     # If hands are present in image(frame) 
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            # Convert landmarks to pixel coordinates
-            landmarks_px = [(int(l.x * img.shape[1]), int(l.y * img.shape[0])) for l in hand_landmarks.landmark]
-
-            # Calculate bounding box
-            x_min = min(landmarks_px, key=lambda x: x[0])[0]
-            y_min = min(landmarks_px, key=lambda x: x[1])[1]
-            x_max = max(landmarks_px, key=lambda x: x[0])[0]
-            y_max = max(landmarks_px, key=lambda x: x[1])[1]
-
-            # Draw bounding box
-            cv2.rectangle(img, (x_min, y_min), (x_max, y_max), (255, 0, 0), 2) 
-
+    if results.multi_hand_landmarks: 
+  
         # Both Hands are present in image(frame) 
         if len(results.multi_handedness) == 2: 
-            # Display 'Both Hands' on the image 
+                # Display 'Both Hands' on the image 
             cv2.putText(img, 'Both Hands', (250, 50), 
                         cv2.FONT_HERSHEY_COMPLEX, 
                         0.9, (0, 255, 0), 2) 
-
+  
         # If any hand present 
         else: 
             for i in results.multi_handedness: 
+                
                 # Return whether it is Right or Left Hand 
                 label = MessageToDict(i)['classification'][0]['label'] 
-
+  
                 if label == 'Left': 
+                    
                     # Display 'Left Hand' on 
                     # left side of window 
                     cv2.putText(img, label+' Hand', 
                                 (20, 50), 
                                 cv2.FONT_HERSHEY_COMPLEX,  
                                 0.9, (0, 255, 0), 2) 
-
+  
                 if label == 'Right': 
+                      
                     # Display 'Left Hand' 
                     # on left side of window 
                     cv2.putText(img, label+' Hand', (460, 50), 
                                 cv2.FONT_HERSHEY_COMPLEX, 
                                 0.9, (0, 255, 0), 2) 
-
+  
     # Display Video and when 'q' 
     # is entered, destroy the window 
     cv2.imshow('Image', img) 
